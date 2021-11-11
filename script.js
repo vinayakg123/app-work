2ef7f155dee5e6ff3abebf12a1220d6a17eaa1df
vinayakg123 <88088473+vinayakg123@users.noreply.github.com>
2021-11-11 21:03:00 +0530

    Edited

---
+++ script.js
@@ -0,0 +1,8 @@
+$(function () { 
+    $('[data-toggle="tooltip"]').tooltip({trigger:'manual'}).tooltip('show');
+  });  
+ 
+    $(".progress-bar").each(function(){
+      each_bar_width = $(this).attr('aria-valuenow');
+      $(this).width(each_bar_width + '%');
+    });
